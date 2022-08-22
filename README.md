37ce78314d217f372a872abcb2e68f8e4692e06c
Kelly Selden <kellyselden@gmail.com>
2022-08-22 16:56:55 -0700

    add script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bitbucket-server-build-status-favicon
+
 Monitor builds using tab icons
+
+Inspired by https://gitlab.com/michal-bryxi/open-source/jenkins-aliceblue.
+
+Available at https://greasyfork.org/en/scripts/450010-bitbucket-server-build-status-favicon.
---
+++ script.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @name        Bitbucket Server Build Status Favicon
+// @namespace   https://github.com/kellyselden
+// @description Monitor builds using tab icons
+// @version     1
+// @author      Kelly Selden
+// supportURL   https://github.com/kellyselden/bitbucket-server-build-status-favicon
+// installURL   https://raw.githubusercontent.com/kellyselden/bitbucket-server-build-status-favicon/main/script.js
+// @license     MIT
+// @match       http*://*bitbucket*/*
+// ==/UserScript==
+
+(() => {
+  const icons = {
+    'build-in-progress-icon': '🔵',
+    'build-successful-icon': '🟢',
+    'build-failed-icon': '🔴',
+  };
+
+  let originalFavicon = document.head.querySelector('link[rel="shortcut icon"]');
+
+  function updateFavicon() {
+    let iconText;
+
+    let status = document.querySelector('[data-testid="pull-request-builds-summary"] .build-status-icon');
+
+    if (status) {
+      for (let [_class, icon] of Object.entries(icons)) {
+        if (status.classList.contains(_class)) {
+          iconText = icon;
+
+          break;
+        }
+      }
+
+      if (!iconText) {
+        iconText = '❓';
+      }
+    }
+
+    let favicon = document.head.querySelector('link[rel="shortcut icon"]');
+
+    if (!iconText) {
+      if (favicon) {
+        document.head.replaceChild(originalFavicon, favicon);
+      }
+
+      return;
+    }
+
+    if (favicon) {
+      document.head.removeChild(favicon);
+
+      favicon = null;
+    }
+
+    if (!favicon) {
+      favicon = document.createElement('link');
+
+      favicon.rel = 'shortcut icon';
+
+      document.head.appendChild(favicon);
+    }
+
+    let svg = document.createElement('svg');
+
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+    let icon = document.createElement('text');
+
+    icon.setAttribute('font-size', '13');
+    icon.setAttribute('y', '13');
+
+    icon.textContent = iconText;
+
+    svg.appendChild(icon);
+
+    favicon.href = `data:image/svg+xml,${svg.outerHTML}`;
+  }
+
+  updateFavicon();
+
+  new MutationObserver(updateFavicon).observe(document.body, {
+    subtree: true,
+    childList: true,
+    attributeFilter: Object.keys(icons),
+  });
+})();

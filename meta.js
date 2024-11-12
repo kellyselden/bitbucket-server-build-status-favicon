472f9d43593ea811139553c3b72679bc57461727
Kelly Selden <kellyselden@gmail.com>
2024-11-12 12:10:41 -0800

    add meta file

---
+++ meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Bitbucket Server Build Status Favicon
+// @namespace   https://github.com/kellyselden
+// @version     1
+// @description Monitor builds using tab icons
+// downloadURL  https://raw.githubusercontent.com/kellyselden/bitbucket-server-build-status-favicon/main/user.js
+// ==/UserScript==
